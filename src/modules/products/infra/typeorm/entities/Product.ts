b40488dfa95334c1b66d53b8ff2cf6e0cc9fe8a8
Michael Moura <mikemorcerf@gmail.com>
2020-05-20 15:20:52 -0400

    create table products and entity product

--- src/modules/products/infra/typeorm/entities/Product.ts
+++ src/modules/products/infra/typeorm/entities/Product.ts
@@ -5,23 +5,35 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   OneToMany,
+  JoinTable,
 } from 'typeorm';
 
 import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
 
+Entity('products');
 class Product {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   name: string;
 
+  @Column()
   price: number;
 
+  @Column()
   quantity: number;
 
+  @OneToMany(type => OrdersProducts, order_product => order_product.product, {
+    cascade: true,
+  })
+  @JoinTable()
   order_products: OrdersProducts[];
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
